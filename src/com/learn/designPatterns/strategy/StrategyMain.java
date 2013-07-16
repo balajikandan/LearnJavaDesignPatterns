b7dd5c4efaad8383be35890c85bb626723e4f294
kbalaji <kbalaji@collab.net>
2013-07-16 18:29:06 +0530

    Adding StrategyMain.java

---
+++ src/com/learn/designPatterns/strategy/StrategyMain.java
@@ -0,0 +1,15 @@
+package com.learn.designPatterns.strategy;
+
+public class StrategyMain {
+
+	/**
+	 * Static main method to invoke and work on the strategy design pattern
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
