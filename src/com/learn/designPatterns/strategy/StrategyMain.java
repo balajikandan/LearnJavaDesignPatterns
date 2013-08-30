79314cee2318f0f3b1a556416cafe595056fa7c0
kbalaji <kbalaji@collab.net>
2013-08-30 14:00:16 +0530

    adding StrategyMain.java and AdaptorMain.java

--- src/com/learn/designPatterns/strategy/StrategyMain.java
+++ src/com/learn/designPatterns/strategy/StrategyMain.java
@@ -1,15 +1,53 @@
 package com.learn.designPatterns.strategy;
 
+
 public class StrategyMain {
 
-	/**
-	 * Static main method to invoke and work on the strategy design pattern
-	 * 
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		HelloWorldStrategy strategy = new UseString();
+		System.out.println(strategy.sayHelloWorld());
+		strategy = new UseStringBuilder(); //LSP design principle
+		System.out.println(strategy.sayHelloWorld());
 	}
-
+	
 }
+
+
+interface HelloWorldStrategy {
+	
+	public String sayHelloWorld();
+	
+}
+
+
+/**
+ * UseString - class is used to construct hello world using String 
+ * Object
+ *  
+ * @author kbalaji
+ *
+ */
+class UseString implements HelloWorldStrategy {
+
+	@Override
+	public String sayHelloWorld() {
+		System.out.println("Strategy 1 - building hello world using String Object");
+		return "Hello World";
+	}
+}
+
+/**
+ * UseStringBuilder - class is used to construct hello world using StringBuilder 
+ * Object
+ * @author kbalaji
+ *
+ */
+class UseStringBuilder implements HelloWorldStrategy {
+	
+	@Override
+	public String sayHelloWorld() {
+		System.out.println("Strategy 2 - building hello world using StringBuilder Object");
+		return new StringBuffer("Hello World").toString();
+	}	
+}
+
